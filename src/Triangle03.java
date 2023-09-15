dee1976b3cfd368a7e284ab45a4eff187ae482e8
Cerivi <antonios.kaharap@gmail.com>
2023-09-15 12:59:51 +0700

    aaaaaaaaadafsa

--- src/Triangle03.java
+++ src/Triangle03.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
 
 public class Triangle03 {
-    Scanner sc = new Scanner(System.in);
-    float base, height, area;
-
-System.out.print("Input base: ");
-    base = sc.nextFloat();
-
-System.out.print("Input height: ");
-    height = sc.nextFloat();
-
-    area = base * height / 2;
-System.out.println("Area of the triangle: " + area);
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int base, height;
+        float area;
+        System.out.print("Input base: ");
+        base = sc.nextInt();
+        System.out.print("Input height: ");
+        height = sc.nextInt();
+        area = (base * height) / 2;
+        System.out.println("Area of the triangle: " + area);
+    }
 }
