f99498909fe8f05d65105f2d1e936a8228ec2e6d
Cerivi <antonios.kaharap@gmail.com>
2023-09-22 15:33:05 +0700

    daspro week 4 (changes again woo 3)

--- Pseudo and flowchard/src/cafeteriaStuff.java
+++ Pseudo and flowchard/src/cafeteriaStuff.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class cafeteriaStuff {
     public static void main (String [] args) {
         Scanner input = new Scanner(System.in);
-        String foodName, drinkName;
+        String foodName, drinkName, currentDate, customerName;
         int foodPrice;
         int drinkPrice;
         int customerServiceFee;
@@ -10,10 +10,19 @@ public class cafeteriaStuff {
         int discFoodDrinkPrice;
         double totalPriceAll;
         double discount, tax = 1.11;
+        boolean MorF, cashOrCredit;
         System.out.println("Input your food: ");
         foodName = input.nextLine();
         System.out.println("Input your drink: ");
         drinkName = input.nextLine();
+        System.out.println("Enter the current date: ");
+        currentDate = input.nextLine();
+        System.out.println("Enter the customer name: ");
+        customerName = input.nextLine();
+        System.out.println("What is your gender ? (false for male | true for female)");
+        MorF = input.nextBoolean();
+        System.out.println("Cash or credit (false for cash | true for credit)");
+        cashOrCredit = input.nextBoolean();
         System.out.println("Input the prices of the food: ");
         foodPrice = input.nextInt();
         System.out.println("Input the prices of the drink: ");
@@ -25,8 +34,11 @@ public class cafeteriaStuff {
         totalPriceFoodDrink = foodPrice + drinkPrice;
         discFoodDrinkPrice = (int) (totalPriceFoodDrink * discount);
         totalPriceAll = (discFoodDrinkPrice + customerServiceFee) * tax;
-        System.out.println("The name of the food and drinks you ordered: \n" + foodName + " " + drinkName);
+        System.out.println("The name of the food and drinks you have ordered: \n" + foodName + "\n " + drinkName);
         System.out.println("The prices are: " + totalPriceAll);
+        System.out.println("The current transaction takes places at: "+ currentDate);
+        System.out.println("The Customer name are: " + " "+ customerName + " " + MorF);
+        System.out.println("Cash or credit (false for cash | true for credit)" + " " + cashOrCredit);
 
     }
 }
