ea9e8f0a963dad50e3cce1bcd9c1db3d47cdcbe1
Cerivi <antonios.kaharap@gmail.com>
2023-10-05 20:00:40 +0700

    daspro week 6 (wow im smart)

---
+++ Selection 2/src/Selection2Exp303.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Selection2Exp303 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String category;
+        int income, netSalary;
+        double tax = 0;
+        System.out.println("Input the category");
+        category = sc.nextLine();
+        System.out.println("Input your income");
+        income = sc.nextInt();
+        if (category.equalsIgnoreCase("workers")) {
+            if (income <= 200000) {
+                tax= 0.1;
+            } else if (income <= 3000000) {
+                tax = 0.15;
+            } else {
+                tax = 0.2;
+            }
+            netSalary = (int) (income - (income*tax));
+            System.out.println(netSalary);
+        } else if (category.equals("businessman")) {
+            if (income <= 2500000) {
+                tax = 0.15;
+            } else if (income <= 3500000) {
+                tax = 0.2;
+            } else {
+                tax = 0.25;
+            }
+            netSalary = (int) (income - (income*tax));
+            System.out.println(netSalary);
+        } else {
+            System.out.println("wrong category");
+        }
+    }
+}
